dd534d948cc390f0d48ada27eac63d061ee6e9a4
SunnyProger <alisha29@mail.ru>
2024-04-04 17:30:43 +0300

    base templates + include inc-menu

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.info),
-    path('item/<int:id>', views.item_info),
-    path('items/', views.get_items),
+    path('', views.home, name='home'),
+    path('about/', views.info, name='about'),
+    path('item/<int:id>', views.item_info, name='item-detail'),
+    path('items/', views.get_items, name='items-list'),
 ]
